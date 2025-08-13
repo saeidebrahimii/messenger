1a214a14b1f4f395bbc1c2fc1601dad59c217c8e
saeidebrahimii <saeidebrahimi.pr@gmail.com>
2025-08-13 17:23:01 +0330

    fix(errorHandler): add console.log for debugging

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 function errorHandler(err, req, res, next) {
+  console.log(err)
   const statusCode = err?.statusCode || 500;
   const message = err?.message || "Internal server error.";
   res.status(statusCode).json({ message });
