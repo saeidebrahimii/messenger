31846b15b1d071907ff0755a5095f8a349b62264
saeidebrahimii <saeidebrahimi.pr@gmail.com>
2025-08-13 12:31:39 +0330

    chore: initial project structure

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const { indexRoutes } = require("./routes");
+const { errorHandler } = require("./middlewares/errorHandler");
+const app = express();
+app.use(express.json());
+
+app.use(indexRoutes);
+app.use((req, res, next) => {
+  res.status(404).json({ message: "route not found" });
+});
+app.use(errorHandler);
+
+module.exports = app;
---
+++ server/src/middlewares/errorHandler.js
@@ -0,0 +1,6 @@
+function errorHandler(err, req, res, next) {
+  const statusCode = err?.statusCode || 500;
+  const message = err?.message || "Internal server error.";
+  res.status(statusCode).json({ message });
+}
+module.exports = { errorHandler };
